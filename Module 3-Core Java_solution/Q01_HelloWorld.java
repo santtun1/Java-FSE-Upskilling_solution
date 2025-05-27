d85457769702dd5d8caa875fa9ccb3517eccacec
krish <santtunray@gmail.com>
2025-05-27 18:12:24 +0530

    rename file name

--- Module 3-Core Java_solution/Q01_HelloWorld.java
+++ Module 3-Core Java_solution/Q01_HelloWorld.java
@@ -2,7 +2,7 @@
 // Objective: Understand the basic structure of a Java program.
 // Task: Write a Java program that prints "Hello, World!" to the console.
 
-public class Q1_HelloWorld {
+public class Q01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
